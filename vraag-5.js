cc42e71b73f119c8ec7d1bffc5ebb52c8a15e4ad
Denperidge <denperidge@gmail.com>
2021-12-21 18:00:02 +0100

    Added vraag 5

---
+++ vraag-5.js
@@ -0,0 +1,13 @@
+// Check of de gebruiker cookies wil ontvangen door hem een melding te sturen die hij kan bevestigen of annuleren
+// De gebruiker mag dus niet alleen amar op ok kunnen klikken
+
+// First of all, "hem", "hij"? Nice cis & male normativity in tech LEERKRACHT SMH
+
+var jaGeantwoord = confirm("COOKIES?");
+// Confirm returned een bool trouwens! So you can use it like this
+if (jaGeantwoord) {
+    alert("Ayo nice, have some!");
+}
+else {
+    alert("That's okay too! Consent is important, and the injustices that are happening nowadays with cases where there is actual proof of a lack of consent but people nevertheless getting away with it due to their privilige is disgusting.\n\nKeep your friends safe, discuss limits with your partners, and don't be afraid to back off from people who don't respect that. Its important. Love you all, stay safe <3");
+}
